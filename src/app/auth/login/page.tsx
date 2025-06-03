72772848a6bbb9445e2b303c5d2e80cc8a7efbb3
nhath24042001 <nhath24042001@gmail.com>
2025-06-03 15:45:09 +0700

    feat: add login and signup pages; update various content files and configurations

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,107 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+export default function LoginPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <div className={cn('flex flex-col gap-6')}>
+          <Card className="overflow-hidden p-0">
+            <CardContent className="grid p-0 md:grid-cols-2">
+              <form className="p-6 md:p-8">
+                <div className="flex flex-col gap-6">
+                  <div className="flex flex-col items-center text-center">
+                    <h1 className="text-2xl font-bold">Welcome back</h1>
+                    <p className="text-muted-foreground text-balance">
+                      Login to your thoughtspace account
+                    </p>
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="email">Email</Label>
+                    <Input id="email" type="email" placeholder="example@gmail.com" required />
+                  </div>
+                  <div className="grid gap-3">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Password</Label>
+                      <a href="#" className="ml-auto text-sm underline-offset-2 hover:underline">
+                        Forgot your password?
+                      </a>
+                    </div>
+                    <Input id="password" type="password" placeholder="******" required />
+                  </div>
+                  <Button type="submit" className="w-full">
+                    Login
+                  </Button>
+                  <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                    <span className="bg-card text-muted-foreground relative z-10 px-2">
+                      Or continue with
+                    </span>
+                  </div>
+                  <div className="grid grid-cols-3 gap-4">
+                    <Button variant="outline" type="button" className="w-full">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                          d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                          fill="currentColor"
+                        />
+                      </svg>
+                      <span className="sr-only">Login with Google</span>
+                    </Button>
+                    <Button variant="outline" type="button" className="w-full">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        className="mr-2"
+                      >
+                        <path d="M12 0C5.37 0 0 5.37 0 12a12.07 12.07 0 0 0 8.21 11.44c.6.11.82-.26.82-.58 0-.29-.01-1.04-.01-2.05-3.34.73-4.04-1.61-4.04-1.61-.55-1.4-1.35-1.77-1.35-1.77-1.1-.75.08-.73.08-.73 1.22.09 1.86 1.26 1.86 1.26 1.08 1.85 2.83 1.32 3.52 1.01.11-.79.42-1.32.76-1.62-2.67-.3-5.47-1.34-5.47-5.96 0-1.32.47-2.4 1.24-3.25-.12-.3-.54-1.52.12-3.18 0 0 1.01-.32 3.3 1.23a11.47 11.47 0 0 1 6 0c2.28-1.55 3.3-1.23 3.3-1.23.66 1.66.24 2.88.12 3.18.77.85 1.23 1.93 1.23 3.25 0 4.63-2.81 5.65-5.49 5.95.43.37.81 1.1.81 2.22 0 1.6-.01 2.88-.01 3.27 0 .32.21.7.82.58A12.07 12.07 0 0 0 24 12c0-6.63-5.37-12-12-12z" />
+                      </svg>
+                      <span className="sr-only">Login with GitHub</span>
+                    </Button>
+                    <Button variant="outline" type="button" className="w-full">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                          d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
+                          fill="currentColor"
+                        />
+                      </svg>
+                      <span className="sr-only">Login with Apple</span>
+                    </Button>
+                  </div>
+
+                  <div className="text-center text-sm">
+                    Don&apos;t have an account?{' '}
+                    <Link href="/auth/signup" className="underline underline-offset-4">
+                      Sign up
+                    </Link>
+                  </div>
+                </div>
+              </form>
+              <div className="bg-muted relative hidden md:block">
+                <Image
+                  src="/bg-login.jpg"
+                  fill
+                  sizes="(max-width: 768px) 100vw, 50vw"
+                  objectFit="cover"
+                  alt="login_bg"
+                />
+              </div>
+            </CardContent>
+          </Card>
+          <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+            By clicking continue, you agree to our <a href="#">Terms of Service</a> and{' '}
+            <a href="#">Privacy Policy</a>.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
